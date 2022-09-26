d2735410564741a9478484192a9466f0904f621b
perehrestvalentina <104031906+perehrestvalentina@users.noreply.github.com>
2022-09-26 10:59:44 +0000

    Deploying to gh-pages from @ perehrestvalentina/goit-js-hw-08-@c42d3ccfb4908a0abcfc9f7a22bd8304c10e87d1 🚀

---
+++ 03-feedback.371b79d7.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".feedback-form");console.log(e);
+//# sourceMappingURL=03-feedback.371b79d7.js.map
---
+++ 03-feedback.e4ce537e.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".feedback-form");console.log(e)}();
+//# sourceMappingURL=03-feedback.e4ce537e.js.map
